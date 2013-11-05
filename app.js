e209fe02b1ead461ca0f1d97d39543db13f16977
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2013-11-05 17:50:08 +0530

    scaffolding

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Express Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+// development only
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler());
+}
+
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+};
+
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', function (req,res,next) {
+	res.send(501);
+});
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Loaded config: ' +  app.get('env'));
+});
