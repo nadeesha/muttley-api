2f3634e3df52cb6f7df236d72b219ef994808e9b
hmudannayake <hemantha.mudannayake@pearson.com>
2013-11-06 12:24:05 +0530

    classroom apis3

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express'),
   http = require('http'),
-  Classroom      = require('./models/classroom'),
-  Article      = require('./models/article'),
+  //Classroom      = require('./models/classroom'),
+  //Article      = require('./models/article'),
+  fs              = require('fs'),
+  	env = process.env.NODE_ENV || 'development',
+	config = require('./config')[env],
+  mongoose = require('mongoose'),
   path = require('path');
+
+// bootstrap db connection
+mongoose.connect(config.db);
+
+var models_path = __dirname + '/models';
+fs.readdirSync(models_path).forEach(function(file) {
+	if (~file.indexOf('.js')) require(models_path + '/' + file)
+});
+  
+var Classroom = mongoose.model('Classroom');
+
 
 var app = module.exports = express();
 
@@ -41,14 +56,14 @@ app.get('/', function (req,res,next) {
 	res.send('hi there',200);
 });
 
-<<<<<<< HEAD
+
 app.get('/classrooms/:classroomId/articles/search', function (req, res, next) {
 	
 });
-=======
+
 app.get('/classrooms', function (req,res,next) {
 	
-	Classroom.findById(identityId, function(error, classrooms) {
+	Classroom.find({},function(error, classrooms) {
 	      if(error) {
 	         res.send(error,500);
 	      }
@@ -60,11 +75,77 @@ app.get('/classrooms', function (req,res,next) {
 });
 
 app.put('/classrooms', function (req,res,next) {
-	res.send('hi there',200);
+	
+
+Classroom.findOne({
+		id: req.classroom.id,
+	}, function(err, result) {
+		if (err) {
+			res.send(500);
+			return next(err);
+		}
+
+		logger.info(req.user.email);
+
+		if (!result) {
+			res.send(500);
+			logger.log('error', 'BETTER_CALL_SAUL: 8FNH46');
+			return next(err);
+		}
+
+		if (profile.firstName !== undefined) result.firstName = profile.firstName;
+		if (profile.lastName !== undefined) result.lastName = profile.lastName;
+		if (profile.dateOfBirth !== undefined) result.dateOfBirth = profile.dateOfBirth;
+		if (profile.gender !== undefined) result.gender = profile.gender;
+		if (profile.location !== undefined) result.location = profile.location;
+		if (profile.contactNumbers !== undefined) result.contactNumbers = profile.contactNumbers;
+		if (profile.nationalIdentifier !== undefined) result.nationalIdentifier = profile.nationalIdentifier;
+		if (profile.isLookingOut !== undefined) result.isLookingOut = profile.isLookingOut;
+		if (profile.languages !== undefined) result.languages = profile.languages;
+		if (profile.tenures !== undefined) result.tenures = profile.tenures;
+		if (profile.skills !== undefined) result.skills = profile.skills;
+		if (profile.qualifications !== undefined) result.qualifications = profile.qualifications;
+
+		result.save(function(err, saved) {
+			if (err && err.name === 'ValidationError') {
+				error = {
+					type: 'Validation',
+					message: err.Messages
+				};
+
+				res.send(400, {
+					error: error
+				});
+
+				logger.log('info', 'Validation error encountered while saving the "user".', {
+					input: JSON.stringify(user),
+					output: JSON.stringify(err)
+				});
+
+				return next(error.message);
+
+			} else if (err) { // if not validation, then this is probably an internal error
+				res.send(500);
+
+
+				logger.log('error', 'Error encountered while saving the following "user".', {
+					input: JSON.stringify(user),
+					output: JSON.stringify(err.Messages)
+				});
+
+				return next(error.output);
+			}
+
+			res.send(200, saved);
+		});
+	});
+
+
+
 });
 
 
->>>>>>> e44ab0ca774c6cf49decb54b2731ed619a4373b2
+
 
 /**
  * Start Server
