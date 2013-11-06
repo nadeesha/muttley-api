2ae6fd8583a5ab244cc3910953202d3e8b9d8e1a
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2013-11-06 19:04:30 +0530

    classroom apis

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 var express = require('express'),
-  http = require('http'),
-  //Classroom      = require('./models/classroom'),
-  //Article      = require('./models/article'),
-  fs              = require('fs'),
-  	env = process.env.NODE_ENV || 'development',
-	config = require('./config')[env],
-  mongoose = require('mongoose'),
-  path = require('path');
+    http = require('http'),
+    fs = require('fs'),
+    process = require('process'),
+    env = process.env.NODE_ENV || 'development',
+    config = require('./config')[env],
+    mongoose = require('mongoose'),
+    path = require('path'),
+    request = require('request'),
+    mashape_key = process.env.MASHAPE_KEY,
+    pm = require('pagemunch');
+
+// pagemunch api key
+pm.set({
+    key: '477eab6e9740bf59ed932bc94d299cc1'
+});
 
 // bootstrap db connection
-console.log("db path %s" , config.db);
+console.log("db path %s", config.db);
 mongoose.connect(config.db);
 
 var models_path = __dirname + '/models';
 fs.readdirSync(models_path).forEach(function(file) {
-	if (~file.indexOf('.js')) require(models_path + '/' + file)
+    if (~file.indexOf('.js')) require(models_path + '/' + file)
 });
-  
+
 var Classroom = mongoose.model('Classroom');
+var Article = mongoose.model('Article');
 
 
 var app = module.exports = express();
@@ -36,12 +44,12 @@ app.use(app.router);
 
 // development only
 if (app.get('env') === 'development') {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 // production only
 if (app.get('env') === 'production') {
-  // TODO
+    // TODO
 };
 
 
@@ -49,142 +57,199 @@ if (app.get('env') === 'production') {
  * Routes
  */
 
-// serve index and view partials
-app.get('/', function (req,res,next) {
-	res.send(501);
+app.get('/', function(req, res, next) {
+    res.send(501);
 });
 
-<<<<<<< HEAD
-=======
+// classrooms
 
->>>>>>> 2aa46283bcf8508ff019cf9b2aef2c0a557cf988
-app.get('/classrooms/:classroomId/articles/search', function (req, res, next) {
-	var classroomId = req.params.classroomId;
-	var q = req.query.q;
+app.get('/classrooms/:classroomId/articles/search', function(req, res, next) {
+    var classroomId = req.params.classroomId;
+    var q = req.query.q;
 
-	// res.send(200, {
-	// 	classroomId: classroomId,
-	// 	q: q
-	// });
+    var q = {
+        query: {
+            fuzzy: {
+                text: req.query.q
+            }
+        }
+    }
 
-	Article.search({"match_all" : { }}, function (err, results) {
-		if (err) 
-			res.send(500, err);
+    Article.search(q, function(err, results) {
+        console.log('searching...');
 
-		res.send(200, event.result)
-	})
+        if (err) {
+            console.log(err);
+            res.send(500, err);
+        }
+
+        res.send(200, results)
+    })
 });
 
-app.get('/classrooms', function (req,res,next) {
-	
-	Classroom.find({},function(error, classrooms) {
-	      if(error) {
-	         res.send(error,500);
-	      }
-	      else {
-			 res.send(classrooms,200);
-	      }
-	  });
-
+app.get('/classrooms', function(req, res, next) {
+    Classroom.find({}, function(error, classrooms) {
+        if (error) {
+            res.send(error, 500);
+        } else {
+            res.send(classrooms, 200);
+        }
+    });
 });
 
-app.put('/classrooms', function (req,res,next) {
-	
-  console.log('doca1');
-    var error,		
-		classroom;
+app.put('/classrooms', function(req, res, next) {
 
-	var croom = new Classroom({
-		name: "foo",
-		url: "bar"
-	});
+    var error,
+        classroom;
 
-	croom.save(function (err, saved) {
-		if (err)
-			res.send(500, err);
-		else
-			res.send(200, saved);
-	})
+    var croom = new Classroom({
+        name: req.body.name
+    });
 
-	// parsin JSON
-	// try {
-	// 	classroom = JSON.parse(req.body.classroom);
-	// } catch (e) {
-	// 	error = {
-	// 		type: 'Validation',
-	// 		message: 'Invalid JSON'
-	// 	};
-
-	// 	res.send(400, error);
-	// 	return next(error.message);
-	// }
-
-
-	// Classroom.findOne({
-	// 	id: req.classroom.id,
-	// }, function(err, result) {
-	// 	if (err) {
-	// 		res.send(500);
-	// 		return next(err);
-	// 	}
-		
-	// 	if (!result) {
-	// 		res.send(500);
-	// 		logger.log('error', 'BETTER_CALL_SAUL: 8FNH46');
-	// 		return next(err);
-	// 	}
-
-	// 	if (classroom.name !== undefined) result.name = classroom.name;
-	// 	if (classroom.url !== undefined) result.url = classroom.url;
-	
-
-	// 	result.save(function(err, saved) {
-	// 		if (err && err.name === 'ValidationError') {
-	// 			error = {
-	// 				type: 'Validation',
-	// 				message: err.Messages
-	// 			};
-
-	// 			res.send(400, {
-	// 				error: error
-	// 			});
-
-	// 			logger.log('info', 'Validation error encountered while saving the "user".', {
-	// 				input: JSON.stringify(classroom),
-	// 				output: JSON.stringify(err)
-	// 			});
-
-	// 			return next(error.message);
-
-	// 		} else if (err) { // if not validation, then this is probably an internal error
-	// 			res.send(500);
-
-
-	// 			logger.log('error', 'Error encountered while saving the following "user".', {
-	// 				input: JSON.stringify(user),
-	// 				output: JSON.stringify(err.Messages)
-	// 			});
-
-	// 			return next(error.output);
-	// 		}
-
-	// 		res.send(200, saved);
-	// 	});
-	// });
-
+    croom.save(function(err, saved) {
+        if (err)
+            res.send(500, err);
+        else
+            res.send(200, saved);
+    });
 });
 
-<<<<<<< HEAD
-=======
+// articles
+
+app.get('/classrooms/:classroomId/articles', function(req, res, next) {
+    Article.find({
+        classroom: req.params.classroomId
+    }, function(error, articles) {
+        if (error) {
+            res.send(500, error);
+        } else {
+            res.send(200, articles);
+        }
+    });
+});
+
+app.put('/classrooms/:classroomId/articles', function(req, res, next) {
+	console.log('----' + JSON.stringify(req.body));
+    var url = req.body.url;
+    var summary, keywords = [];
+
+    var options = {
+        uri: 'https://tldr.p.mashape.com/summary',
+        qs: {
+            url: url
+        },
+        headers: {}
+    }
+
+    options.headers["X-Mashape-Authorization"] = mashape_key;
 
 
+    request.get(options, function(err, r, body) {
+        if (err)
+            res.send(500, err);
 
->>>>>>> 2aa46283bcf8508ff019cf9b2aef2c0a557cf988
+        body = JSON.parse(body);
+
+        console.log('Got summary');
+
+        console.log('-----', body);
+
+        summary = body.data.summary;
+        keywords = body.data.keywords;
+
+        var article = new Article({
+            url: url,
+            summary: summary,
+            keywords: keywords,
+            classroom: req.params.classroomId
+        });
+
+        console.log(article);
+
+        var options = {
+            uri: 'https://alchemy.p.mashape.com/url/URLGetTitle',
+            qs: {
+                url: url,
+                outputMode: "json"
+            },
+            headers: {}
+        }
+
+        options.headers["X-Mashape-Authorization"] = mashape_key;
+
+        // pagemunch summary
+        request.get(options, function(err, r, body) {
+            if (err) {
+                res.send(500, err);
+                console.log(err);
+            } else {
+        		body = JSON.parse(body);
+                console.log('Got title');
+        		console.log('-----', body);
+                article.title = body.title;
+        		console.log(article);
+
+                var options = {
+                    url: "https://alchemy.p.mashape.com/url/URLGetText",
+                    qs: {
+                        url: url,
+                        outputMode: "json"
+                    },
+                    headers: {}
+                }
+
+                options.headers["X-Mashape-Authorization"] = mashape_key;
+
+                request.get(options, function(err, r, body) {
+                    if (err)
+                        res.send(500, err);
+
+                    body = JSON.parse(body);
+
+                    console.log('Got raw text');
+        			console.log('-----', body);
+                    article.text = body.text;
+        			console.log(article);
+
+                    article.save(function(err, obj) {
+                    	if (err)
+                    		res.send(500, err);
+
+                        res.send(201, {
+                            id: obj._id
+                        });
+                    });
+                });
+            }
+        })
+    });
+});
+
+app.del('/classrooms/:classroomId/articles/:articleId', function(req, res, next) {
+    Article.remove({
+        classroom: classroomId,
+        _id: articleId
+    }, function(err) {
+        if (err)
+            res.send(500, err);
+        else
+            res.send(200);
+    })
+
+    res.send(501);
+});
+
 /**
  * Start Server
  */
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-  console.log('Loaded config: ' +  app.get('env'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Loaded config: ' + app.get('env'));
+
+    if (mashape_key) {
+        console.log("Mashape key is set");
+    } else {
+        console.log('Mashape key is not set');
+    }
 });
