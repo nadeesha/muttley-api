ebfe9d4f67b605d7a42d9f7c4b9ada6c2cef544e
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2013-11-06 12:06:42 +0530

    mongo cluster

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	development: {
+		db: 'mongodb://heroku_app19231043:muttley1q@ds053948.mongolab.com:53948/heroku_app19231043',
+		elasticsearch: {
+                host: 'uguyq6rk:thag4edxspa9tdf4@elm-8960080.eu-west-1.bonsai.io',
+                port: 80
+        }
+	},
+	production: {
+		db: 'mongodb://heroku_app19231043:muttley1q@ds053948.mongolab.com:53948/heroku_app19231043',
+		elasticsearch: {
+                host: 'uguyq6rk:thag4edxspa9tdf4@elm-8960080.eu-west-1.bonsai.io',
+                port: 80
+        }
+	}
+}
